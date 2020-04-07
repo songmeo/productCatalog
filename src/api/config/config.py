bda2bdd8f1d8f4dc3677867d654088063b795b99
songmeo <shuong1012@gmail.com>
2020-04-07 11:26:34 +0300

    fix testingconfg

--- src/api/config/config.py
+++ src/api/config/config.py
@@ -13,20 +13,4 @@ class DevelopmentConfig(Config):
 	
 class TestingConfig(Config):
 	TESTING = True
-	SQLALCHEMY_DATABASE_URI = 'sqlite:////home/kat/adcash/database/products.db'
 	SQLALCHEMY_ECHO = False
-
-class TestingConfig(Config):
-	TESTING = True
-	SQLALCHEMY_ECHO = False
-	JWT_SECRET_KEY = 'JWT-SECRET'
-	SECRET_KEY = 'SECRET-KEY'
-	SECURITY_PASSWORD_SALT = 'PASSWORD-SALT'
-	MAIL_DEFAULT_SENDER= ''
-	MAIL_SERVER= 'smtp.gmail.com'
-	MAIL_PORT= 465
-	MAIL_USERNAME= ''
-	MAIL_PASSWORD= ''
-	MAIL_USE_TLS= False
-	MAIL_USE_SSL= True
-	UPLOAD_FOLDER= 'images'
